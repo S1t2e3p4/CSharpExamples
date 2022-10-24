79b67813cfdcd8c989d39e7e6279081e0afcf10e
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-10-24 14:02:03 +0300

    написал код по  задаче1 и задаче2

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,10 +1,16 @@
-﻿// Нартштте программу, которая на вход принимает число и
-// выдаёт его квадрат (число умноженное на само себя)
-// Например:
-// 4 -> 16
-// -3 -> 9
-// -7 -> 49
-Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine());
-int sqr = number*number;
-Console.WriteLine (" Квадрат числа " + number + " равен = " + sqr);
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Нартштте программу, которая на вход принимает число и
+        // выдаёт его квадрат (число умноженное на само себя)
+        // Например:
+        // 4 -> 16
+        // -3 -> 9
+        // -7 -> 49
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+        int sqr = number * number;
+        Console.WriteLine(" Квадрат числа " + number + " равен = " + sqr);
+    }
+}
--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, котрая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// Например:
+// а=25, b=5 -> Да
+// а=2, b=10 -> Нет
+// а=9, b= -3 -> Да
+// а= -3, b=9 -> Нет
+Console.WriteLine("Введите первое число");
+int numberOne = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Второе число");
+int numberTwo = int.Parse(Console.ReadLine());
+if ( numberOne == numberTwo*numberTwo)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+  Console.WriteLine("Нет");  
+}
