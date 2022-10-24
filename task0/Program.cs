89cf5938aaa5de02ab4b93d3d2ebd05b7627abc1
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-10-24 23:03:43 +0300

    исправил код для решения залачи0

--- task0/Program.cs
+++ task0/Program.cs
@@ -8,7 +8,7 @@
         // 4 -> 16
         // -3 -> 9
         // -7 -> 49
-        Console.Write("Введите число: ");
+        Console.WriteLine ("Введите число: ");
         int number = int.Parse(Console.ReadLine());
         int sqr = number * number;
         Console.WriteLine(" Квадрат числа " + number + " равен = " + sqr);
