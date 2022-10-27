0ed6cc27dfc3bbb682a3b271e51a0f292cf292fc
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-10-27 20:46:56 +0300

    написал код для решения залачи5

--- task5/Program.cs
+++ task5/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// Например:
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+Console.Clear ();
+int num = new Random(). Next (10, 100);
+int a1 = num/10;
+int a2 = num%10;
+int max = a1;
+Console.WriteLine($"{a1}, {a2}");
+if (max<a2)
+{
+    max=a2;
+}
+Console.WriteLine($"Максимальная цифра числе {num} - > {max}");
