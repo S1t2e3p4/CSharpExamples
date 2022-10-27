392ad97a5a51446e2d6a5cc15f7e12b6cf94e11c
Stepuro Mikhail <s1t2e3p4@mail.ru>
2022-10-27 21:31:23 +0300

    написал код для решения залачи6

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// Например:
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+Console.Clear ();
+int num = new Random(). Next (100, 1000);
+int a1 = num/100;
+int a2 = num%10;
+int count1 = a1*10;
+int count = count1+a2;
+Console.WriteLine($"Число a1={a1}, число a2={a2}");
+Console.WriteLine($"Число {num} сократилось до {count}");
